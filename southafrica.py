33f91abd91d73afeaed9441bfa5cbb51f7c2f732
AndiswaCyria <andiswacyriam@gmail.com>
2025-03-04 20:49:52 +0200

    We are about to clone

--- southafrica.py
+++ southafrica.py
@@ -1,2 +1,4 @@
 print =("my work may have disappered, what the heck")
 print("I will try to modify this project, then commit it on terminal, and see if it will register on github without commitingon vs code")
+
+print('clone')
