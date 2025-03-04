4ce00a3e04b945b0da43c64e87933fd899e2f0e9
AndiswaCyria <andiswacyriam@gmail.com>
2025-03-04 20:33:52 +0200

    Finally got it, commit via terminal, save changes on vs code first

--- southafrica.py
+++ southafrica.py
@@ -1,1 +1,2 @@
 print =("my work may have disappered, what the heck")
+print("I will try to modify this project, then commit it on terminal, and see if it will register on github without commitingon vs code")
